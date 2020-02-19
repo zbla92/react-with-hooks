b58b038f910d1a6a39e7f9e58e8b18f468f36a23
Zbla92 <milanblaz@live.com>
2020-02-19 17:25:23 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useForm } from './useForm';
 
 function App() {
+  const [values, handleChange] = useForm({
+    email: '',
+    password: '',
+    firstName: ''
+  });
+
+  useEffect(() => {
+    console.log('render');
+  }, [values.password, values.firstName]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App container'>
+      <input
+        name='email'
+        placeholder='Email'
+        value={values.email}
+        onChange={handleChange}
+      />
+      <input
+        name='firstName'
+        placeholder='First Name'
+        value={values.firstName}
+        onChange={handleChange}
+      />
+      <input
+        type='password'
+        name='password'
+        value={values.password}
+        onChange={handleChange}
+        placeholder='Password'
+      />
     </div>
   );
 }
